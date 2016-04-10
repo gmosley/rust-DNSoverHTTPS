565f895f3f698f3ef0e4286e18a4d6a9c050eb9a
Graham Mosley <gmosley@seas.upenn.edu>
2016-04-10 16:09:43 -0400

    basic google HTTPS request

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 extern crate dns_parser;
+extern crate hyper;
+
+use std::io::Read;
 
 use std::net::UdpSocket;
 use dns_parser::{Packet, Name};
 
+use hyper::Client;
+use hyper::header::{Connection, Host};
+
 fn main() {
+
+    make_request();
 
     let socket = UdpSocket::bind("127.0.0.1:53").unwrap();
 
@@ -19,10 +27,28 @@ fn main() {
                 let buf = &mut buf[..amt];
                 let packet = Packet::parse(&buf).unwrap();
                 if packet.questions.len() == 1 {
-                    println!("{}\n", &packet.questions[0].qname.to_string());
+                    let question = &packet.questions[0];
+                    println!("{} {:?}\n", &question.qname.to_string(), question.qtype);
                 }
             }
             Err(e) => panic!("Error receiving datagram: {}", e),
         }
     }
+}
+
+fn make_request() {
+    let mut client = Client::new();
+
+    let mut res = client.get("https://4.31.115.237/resolve?name=seas.upenn.edu.")
+        .header(Host{
+            hostname: "dns.google.com".to_owned(),
+            port: None,
+        })
+        .header(Connection::close())
+        .send().unwrap();
+
+    let mut body = String::new();
+    res.read_to_string(&mut body).unwrap();
+
+    println!("Response: {}", body);
 }
